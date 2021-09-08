a9f41e24ee70689c80cbba55a93bbd8829ae98ae
Nic Luginbühl <nic.luginbuehl@agilita.ch>
2021-09-08 16:44:37 +0200

    commit day2

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,4 @@
-StereoPannerNode.ui.define([
-"sap/ui/core/UIComponent"
-] , function (UIComponent){
-    "use strict";
-    return UIComponent.extend("opensap.myapp.Component", {            
-        metadata : {
-            manifest: "json"
-        },
-
-        init : function () {
-            UIComponent.prototype.init.apply(this, arguments);                        
-        }
-    });
-});sap.ui.define([
+sap.ui.define([
 	"sap/ui/core/UIComponent"
 ], function (UIComponent) {
 	"use strict";
